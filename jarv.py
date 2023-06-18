dacc8fb7ae635b546b0c443d6ba2033d00b608d4
saad habibi <116023617+saadhabibi@users.noreply.github.com>
2023-06-18 20:10:20 +0530

    Update jarv.py

--- jarv.py
+++ jarv.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
+                to = "saadhabibi65mail@gmail.com"    
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
